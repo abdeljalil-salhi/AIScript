714676880ffa2a547913952650f05382e6890d8f
Abdel <abdelisloading@gmail.com>
2024-03-11 06:11:04 +0000

    Add basic styling rules to tailwindcss config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-import "./App.css";
+// Dependencies
+import { FC } from "react";
 
-function App() {
+// Interfaces
+interface AppProps {}
+
+// Component
+export const App: FC<AppProps> = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
-
-export default App;
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Dependencies
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// Styles
+import "./index.css";
+
+// Components
+import { App } from "./App";
+
+/**
+ * Main component that renders the entire application.
+ */
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);

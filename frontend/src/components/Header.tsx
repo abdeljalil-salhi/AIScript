4e1d985a31636241f2a341a491f2e1ab713a32a0
Abdel <abdelisloading@gmail.com>
2024-03-11 09:18:59 +0000

    Add header for large devices

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,16 @@
 // Dependencies
 import { FC } from "react";
+import { useLocation } from "react-router-dom";
 
+// SVGs
 import brainwave from "../assets/brainwave.svg";
+
+// Constants
+import { navigations } from "../constants";
+import { Navigation } from "../constants/types";
+
+// Components
+import { Button } from "./Button";
 
 // Interfaces
 interface HeaderProps {}
@@ -14,12 +23,42 @@ interface HeaderProps {}
  * @exports Header
  */
 export const Header: FC<HeaderProps> = (): JSX.Element => {
+  const pathname = useLocation();
+
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="AIScript" />
         </a>
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigations.map((item: Navigation) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </nav>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
       </div>
     </div>
   );
---
+++ frontend/src/constants/types.d.ts
@@ -0,0 +1,6 @@
+export interface Navigation {
+  id: string;
+  title: string;
+  url: string;
+  onlyMobile?: boolean;
+}

c308aa2e92f8ce4a6dbf14ad94e2dba76d1ac2a9
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-03-13 21:41:25 +0000

    Add collaboration circle with the logo in the center

--- frontend/src/components/Collaboration.tsx
+++ frontend/src/components/Collaboration.tsx
@@ -2,9 +2,9 @@
 import { FC } from "react";
 
 // Assets
-import { check } from "../assets";
+import { brainwaveSymbol, check } from "../assets";
 // Constants
-import { collabContent } from "../constants";
+import { collabContent, collabText } from "../constants";
 // Components
 import { Button } from "./Button";
 import { Section } from "./Section";
@@ -47,6 +47,25 @@ export const Collaboration: FC<CollaborationProps> = (): JSX.Element => {
             ))}
           </ul>
           <Button>Try it now</Button>
+        </div>
+        <div className="lg:ml-auto xl:w-[38rem] mt-4">
+          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
+          </p>
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    width={48}
+                    height={48}
+                    alt="AIScript"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Section>

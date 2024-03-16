3548a86eb89efafb45d959333da49813a00af8f0
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-03-16 02:53:49 +0000

    Add Smartest AI service to the bento box section

---
+++ frontend/src/components/Services.tsx
@@ -0,0 +1,72 @@
+// Dependencies
+import { FC } from "react";
+
+// Constants
+import { brainwaveServices } from "../constants";
+// Assets
+import { check, service1 } from "../assets";
+// Components
+import { Generating } from "./Generating";
+import { Heading } from "./Heading";
+import { Section } from "./Section";
+
+// Interfaces
+interface ServicesProps {}
+
+/**
+ * Services Component
+ *
+ * @interface ServicesProps
+ * @returns {JSX.Element} - Services Component
+ * @exports Services
+ */
+export const Services: FC<ServicesProps> = (): JSX.Element => {
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for authors."
+          text="AIScript unlocks the potential of AI-powered books."
+        />
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+              <img
+                src={service1}
+                width={800}
+                height={730}
+                className="w-full h-full object-cover md:object-right"
+                alt="Smartest AI"
+                draggable={false}
+              />
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                AIScript unlocks the potential of AI-powered e-books.
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((service, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img
+                      src={check}
+                      width={24}
+                      height={24}
+                      alt="Check icon"
+                      draggable={false}
+                    />
+                    <p className="ml-4">{service}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};

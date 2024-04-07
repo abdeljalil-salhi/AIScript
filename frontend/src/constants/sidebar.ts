94e2f0bee5603f0c1ddbc6a2f4b781394a43ed0d
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-04-07 03:55:17 +0100

    Add dashboard layout - sidebar

---
+++ frontend/src/constants/sidebar.ts
@@ -0,0 +1,20 @@
+// Interfaces
+import { NavigationLink } from "./types";
+
+export const navigationLinks: NavigationLink[] = [
+  {
+    id: "0",
+    title: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    id: "1",
+    title: "Create",
+    href: "/create",
+  },
+  {
+    id: "2",
+    title: "Library",
+    href: "/library",
+  },
+];

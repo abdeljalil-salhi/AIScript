84ae7b44862b4f477c13f586fccbe6dfb6931fb4
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-04-22 20:47:32 +0100

    Add cover component

--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,5 +1,8 @@
 // Dependencies
-import { FC } from 'react';
+import { FC } from "react";
+
+// Components
+import { Cover } from "./Cover";
 
 // Interfaces
 interface CreatePageProps {}
@@ -12,9 +15,12 @@ interface CreatePageProps {}
  * @exports CreatePage
  */
 export const CreatePage: FC<CreatePageProps> = (): JSX.Element => {
-    return (
-        <div className="">
-            create page
-        </div>
-    );
+  return (
+    <div className="w-full flex flex-row">
+      <div className="w-1/3 min-h-screen border-r border-n-6/70 p-4">
+        <Cover />
+      </div>
+      <div className="w-2/3 min-h-screen p-4"></div>
+    </div>
+  );
 };

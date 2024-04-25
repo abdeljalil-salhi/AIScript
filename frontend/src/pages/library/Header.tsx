3eab56381049e828bc4255b74ed0fd4a793c3c78
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-04-26 00:44:26 +0100

    Add library page and refactor list display component

---
+++ frontend/src/pages/library/Header.tsx
@@ -0,0 +1,20 @@
+// Dependencies
+import { FC } from "react";
+
+// Interfaces
+interface HeaderProps {}
+
+/**
+ * Header Component
+ *
+ * @interface HeaderProps
+ * @returns {JSX.Element} - Header Component
+ * @exports Header
+ */
+export const Header: FC<HeaderProps> = (): JSX.Element => {
+  return (
+    <header className="p-4 md:p-6 w-full">
+      <h2 className="font-semibold text-4xl">Library</h2>
+    </header>
+  );
+};

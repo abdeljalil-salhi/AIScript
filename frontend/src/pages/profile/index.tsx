5dfc8d9e084a1903c8e58531b8b1ce7c43ec90ed
Abdel <abdelisloading@gmail.com>
2024-05-01 19:15:13 +0100

    Add verify email address modal

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -17,11 +17,11 @@ interface ProfilePageProps {}
  */
 export const ProfilePage: FC<ProfilePageProps> = (): JSX.Element => {
   return (
-    <div className="w-full h-[calc(100vh-3.5rem)] md:h-screen flex flex-col md:flex-row justify-center gap-6 overflow-y-auto p-4 md:p-6 font-['Poppins']">
-      <div className="w-full md:w-2/5 flex flex-col items-center justify-start pt-36 md:pt-6">
+    <div className="w-full h-[calc(100vh-3.5rem)] md:h-screen flex flex-col md:flex-row justify-center gap-6 lg:gap-32 overflow-y-auto p-4 md:p-6 lg:p-16 font-['Poppins']">
+      <div className="w-full md:w-2/5 flex flex-col items-center lg:items-end justify-start pt-36 md:pt-6">
         <ProfilePicture />
       </div>
-      <div className="w-full md:w-3/5 h-full flex flex-col items-center justify-start gap-6 p-4 md:pt-6">
+      <div className="w-full md:w-3/5 h-full flex flex-col items-center lg:items-start justify-start gap-6 p-4 md:pt-6">
         <SettingsForm />
       </div>
     </div>

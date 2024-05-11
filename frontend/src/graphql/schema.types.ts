015da206ca2ef66d9376a2dcd779e11e4266d7ad
Abdel <abdelisloading@gmail.com>
2024-05-11 03:44:04 +0100

    Update GraphQL schema types using codegen

--- frontend/src/graphql/schema.types.ts
+++ frontend/src/graphql/schema.types.ts
@@ -99,14 +99,60 @@ export type MeResponse = {
 };
 
 export type Mutation = {
+  /** Cancels a subscription entity by ID. */
+  cancelSubscription: Subscription;
+  /** Creates a new payment entity. */
+  createPayment: Payment;
+  /** Creates the predefined plans in the database. */
+  createPlans: Array<Plan>;
+  /** Creates a new subscription entity. */
+  createSubscription: Subscription;
+  /** Deletes all plans from the database. */
+  deleteAllPlans: Array<Plan>;
+  /** Deletes a plan by its ID. */
+  deletePlan: Plan;
+  /** Deletes a subscription entity by ID. */
+  deleteSubscriptionById: Subscription;
+  /** Deletes all subscription entities for a specific user. */
+  deleteSubscriptionsByUserId: Array<Subscription>;
   /** Logs in a user with the specified details. */
   login: AuthResponse;
   /** Logs out a user with the specified user ID. */
   logout: LogoutResponse;
   /** Generates new access and refresh tokens for a user. */
   refresh: NewTokensResponse;
+  /** Refreshes all subscriptions credits. */
+  refreshSubscriptionsCredits: Scalars["String"]["output"];
   /** Registers a new user with the specified details. */
   register: AuthResponse;
+  /** Updates a plan by its ID. */
+  updatePlan: Plan;
+  /** Validates all subscriptions. */
+  validateSubscriptions: Scalars["String"]["output"];
+};
+
+export type MutationCancelSubscriptionArgs = {
+  subscriptionId: Scalars["String"]["input"];
+};
+
+export type MutationCreatePaymentArgs = {
+  newPaymentInput: NewPaymentInput;
+};
+
+export type MutationCreateSubscriptionArgs = {
+  newSubscriptionInput: NewSubscriptionInput;
+};
+
+export type MutationDeletePlanArgs = {
+  planId: Scalars["String"]["input"];
+};
+
+export type MutationDeleteSubscriptionByIdArgs = {
+  subscriptionId: Scalars["String"]["input"];
+};
+
+export type MutationDeleteSubscriptionsByUserIdArgs = {
+  userId: Scalars["String"]["input"];
 };
 
 export type MutationLoginArgs = {
@@ -117,6 +163,35 @@ export type MutationRegisterArgs = {
   registerInput: RegisterInput;
 };
 
+export type MutationUpdatePlanArgs = {
+  planId: Scalars["String"]["input"];
+  updatePlanInput: UpdatePlanInput;
+};
+
+export type NewPaymentInput = {
+  /** Amount of the payment */
+  amount: Scalars["Float"]["input"];
+  /** Order ID of the payment */
+  orderId: Scalars["String"]["input"];
+  /** Source of the payment */
+  paymentSource: Scalars["String"]["input"];
+  /** PayPal subscription ID of the payment */
+  paypalSubId: Scalars["String"]["input"];
+  /** ID of the associated plan that the payment is for */
+  planId: Scalars["String"]["input"];
+  /** ID of the associated user that owns the payment */
+  userId: Scalars["String"]["input"];
+};
+
+export type NewSubscriptionInput = {
+  /** ID of the payment that created the subscription */
+  paymentId: Scalars["String"]["input"];
+  /** ID of the subscription plan */
+  planId: Scalars["String"]["input"];
+  /** ID of the associated user that owns the subscription */
+  userId: Scalars["String"]["input"];
+};
+
 export type NewTokensResponse = {
   /** JSON Web Token (JWT) used for authorization and access */
   accessToken: Scalars["String"]["output"];
@@ -124,11 +199,83 @@ export type NewTokensResponse = {
   refreshToken: Scalars["String"]["output"];
 };
 
+export type Payment = {
+  /** Amount of the payment */
+  amount: Scalars["Float"]["output"];
+  /** Date and time of the payment creation */
+  createdAt: Scalars["DateTime"]["output"];
+  /** Unique identifier of the payment */
+  id: Scalars["String"]["output"];
+  /** Order ID of the payment */
+  orderId: Scalars["String"]["output"];
+  /** Source of the payment */
+  paymentSource: Scalars["String"]["output"];
+  /** PayPal subscription ID of the payment */
+  paypalSubId: Scalars["String"]["output"];
+  /** Payment associated subscription entity */
+  subscription?: Maybe<Scalars["String"]["output"]>;
+  /** Subscription ID of the payment */
+  subscriptionId: Scalars["String"]["output"];
+  /** Date and time of the payment last update */
+  updatedAt: Scalars["DateTime"]["output"];
+  /** Payment associated user entity */
+  user?: Maybe<User>;
+  /** ID of the associated user that owns the payment */
+  userId: Scalars["String"]["output"];
+};
+
+export type Plan = {
+  /** Date and time of the plan creation */
+  createdAt: Scalars["DateTime"]["output"];
+  /** Number of credits given by the plan */
+  credits: Scalars["Float"]["output"];
+  /** Description of the plan */
+  description: Scalars["String"]["output"];
+  /** Duration of the plan */
+  duration: Scalars["Float"]["output"];
+  /** Unique identifier of the plan */
+  id: Scalars["String"]["output"];
+  /** Name of the plan */
+  name: Scalars["String"]["output"];
+  /** Price of the plan */
+  price: Scalars["Float"]["output"];
+  /** Date and time of the plan last update */
+  updatedAt: Scalars["DateTime"]["output"];
+};
+
 export type Query = {
+  /** Gets all the available plans. */
+  getAllPlans: Array<Plan>;
+  /** Retrieves all subscription entities. */
+  getAllSubscriptions: Array<Subscription>;
+  /** Gets a plan by its ID. */
+  getPlanById: Plan;
+  /** Retrieves a single subscription entity by ID. */
+  getSubscriptionById: Subscription;
+  /** Retrieves all subscription entities for a specific plan. */
+  getSubscriptionsByPlanId: Array<Subscription>;
+  /** Retrieves all subscription entities for a specific user. */
+  getSubscriptionsByUserId: Array<Subscription>;
   /** A simple hello world query to test the GraphQL API. */
   hello: Scalars["String"]["output"];
   /** Returns the currently authenticated user. */
   me: MeResponse;
+};
+
+export type QueryGetPlanByIdArgs = {
+  planId: Scalars["String"]["input"];
+};
+
+export type QueryGetSubscriptionByIdArgs = {
+  subscriptionId: Scalars["String"]["input"];
+};
+
+export type QueryGetSubscriptionsByPlanIdArgs = {
+  planId: Scalars["String"]["input"];
+};
+
+export type QueryGetSubscriptionsByUserIdArgs = {
+  userId: Scalars["String"]["input"];
 };
 
 export type RegisterInput = {
@@ -140,6 +287,48 @@ export type RegisterInput = {
   password: Scalars["String"]["input"];
   /** Username of the user */
   username: Scalars["String"]["input"];
+};
+
+export type Subscription = {
+  /** Date and time of the subscription creation */
+  createdAt: Scalars["DateTime"]["output"];
+  /** Number of days with service ongoing */
+  daysWithService: Scalars["Float"]["output"];
+  /** Unique identifier of the subscription */
+  id: Scalars["String"]["output"];
+  /** Subscription is active */
+  isActive: Scalars["Boolean"]["output"];
+  /** Subscription is deactivated */
+  isDeactivated: Scalars["Boolean"]["output"];
+  /** ID of the owner user that owns the subscription */
+  ownerUserId?: Maybe<Scalars["String"]["output"]>;
+  /** Subscription associated payment entity */
+  payment?: Maybe<Payment>;
+  /** ID of the associated payment that owns the subscription */
+  paymentId: Scalars["String"]["output"];
+  /** Subscription associated plan entity */
+  plan?: Maybe<Plan>;
+  /** ID of the subscription plan */
+  planId: Scalars["String"]["output"];
+  /** Date and time of the subscription last refresh */
+  refreshedAt: Scalars["DateTime"]["output"];
+  /** Date and time of the subscription last update */
+  updatedAt: Scalars["DateTime"]["output"];
+  /** Subscription associated user entity */
+  user?: Maybe<User>;
+  /** ID of the associated user that owns the subscription */
+  userId?: Maybe<Scalars["String"]["output"]>;
+};
+
+export type UpdatePlanInput = {
+  /** Plan description */
+  description?: InputMaybe<Scalars["String"]["input"]>;
+  /** Plan duration */
+  duration?: InputMaybe<Scalars["Int"]["input"]>;
+  /** Plan name */
+  name?: InputMaybe<Scalars["String"]["input"]>;
+  /** Plan price */
+  price?: InputMaybe<Scalars["Int"]["input"]>;
 };
 
 export type User = {
@@ -155,6 +344,8 @@ export type User = {
   isAdmin: Scalars["Boolean"]["output"];
   /** Refresh token of the user */
   refreshToken?: Maybe<Scalars["String"]["output"]>;
+  /** User associated subscription entity */
+  subscription?: Maybe<Subscription>;
   /** Date and time of the user last update */
   updatedAt: Scalars["DateTime"]["output"];
   /** Username of the user */
@@ -168,8 +359,14 @@ export type Wallet = {
   balance: Scalars["Float"]["output"];
   /** Date and time of the wallet creation */
   createdAt: Scalars["DateTime"]["output"];
+  /** Number of free credits available in the wallet */
+  freeCredits: Scalars["Float"]["output"];
   /** Unique identifier of the wallet */
   id: Scalars["String"]["output"];
+  /** Number of subscription credits available in the wallet */
+  subscriptionCredits: Scalars["Float"]["output"];
+  /** Number of top-up credits available in the wallet */
+  topUpCredits: Scalars["Float"]["output"];
   /** Date and time of the wallet last update */
   updatedAt: Scalars["DateTime"]["output"];
   /** Wallet associated user entity */

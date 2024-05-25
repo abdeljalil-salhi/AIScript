922ba8da570a9107720a214602c0005b516858e6
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-05-25 20:16:56 +0100

    Add queuing prototype to the create page

--- frontend/src/pages/create/CreateForm.tsx
+++ frontend/src/pages/create/CreateForm.tsx
@@ -1,5 +1,7 @@
 // Dependencies
-import { FC } from "react";
+import { FC, useContext } from "react";
+import { Spin } from "antd";
+import { useGetIdentity } from "@refinedev/core";
 
 // Components
 import { ChaptersAndSections } from "@/components/create/ChaptersAndSections";
@@ -8,6 +10,21 @@ import { Cover } from "@/components/create/Cover";
 import { suggestions } from "@/constants/home";
 // Utils
 import { secureRandomIndexes } from "@/utils/secureRandomIndexes";
+// Contexts
+import { SocketContext } from "@/contexts/socket";
+import { SocketContext as ISocketContext } from "@/contexts/socket/interfaces/socket-context.interface";
+// GraphQL Types
+import { MeResponse } from "@/graphql/schema.types";
+// Utils
+import { isFreePlan } from "@/utils/isFreePlan";
+
+/**
+ * @description
+ * The websocket connection.
+ * It is used to emit and listen to events.
+ * @type {Socket<ServerToClientEvents, ClientToServerEvents>}
+ */
+import { ws } from "@/sockets";
 
 // Interfaces
 interface CreateFormProps {}
@@ -20,11 +37,70 @@ interface CreateFormProps {}
  * @exports CreateForm
  */
 export const CreateForm: FC<CreateFormProps> = (): JSX.Element => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  /**
+   * Use the socket context to get the queue informations.
+   */
+  const { queue } = useContext<ISocketContext>(SocketContext);
+
+  /**
+   * Get the user's identity
+   * @type {MeResponse}
+   */
+  const { data: identity, isLoading: isIdentityLoading } =
+    useGetIdentity<MeResponse>();
+
+  /**
+   * Handle the form submission.
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e - The form event.
+   * @returns {void}
+   */
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+
+    // If the identity is loading or not available, stop the form submission.
+    if (isIdentityLoading || !identity) return;
+
+    console.log("CreateForm handleSubmit");
+
+    if (isFreePlan(identity.user.subscription?.plan))
+      ws.emit("joinSharedQueue");
+    else ws.emit("joinPriorityQueue");
   };
 
-  return (
+  console.log(queue);
+
+  return queue.isChecking ? (
+    <div className="w-full h-full flex items-center justify-center font-['Poppins']">
+      <Spin />
+    </div>
+  ) : queue.inQueue ? (
+    <div className="w-full flex flex-col gap-3 font-['Poppins']">
+      <div className="w-full flex flex-col gap-1">
+        <label>Queue</label>
+        <p>
+          {queue.positionInQueue === 1
+            ? "Please wait while we create your book."
+            : `You are in position ${queue.positionInQueue} in the queue.`}
+        </p>
+        {queue.positionInQueue !== 1 && (
+          <button
+            onClick={() => {
+              if (isIdentityLoading || !identity) return;
+
+              if (isFreePlan(identity.user.subscription?.plan))
+                ws.emit("leaveSharedQueue");
+              else ws.emit("leavePriorityQueue");
+
+              console.log("Leave queue");
+            }}
+          >
+            Leave queue
+          </button>
+        )}
+      </div>
+    </div>
+  ) : (
     <form
       className="w-full flex flex-col gap-3 font-['Poppins']"
       onSubmit={handleSubmit}

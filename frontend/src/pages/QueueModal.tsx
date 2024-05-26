6890e7e5fc0dc00b890a015e322f09916e84cfcd
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-05-26 01:39:46 +0100

    Hide queuing modal on disconnect and show on reconnect

--- frontend/src/pages/QueueModal.tsx
+++ frontend/src/pages/QueueModal.tsx
@@ -1,5 +1,5 @@
 // Dependencies
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { useGetIdentity } from "@refinedev/core";
 
@@ -8,9 +8,17 @@ import { SocketContext } from "@/contexts/socket";
 import { SocketContext as ISocketContext } from "@/contexts/socket/interfaces/socket-context.interface";
 // GraphQL Types
 import { MeResponse } from "@/graphql/schema.types";
-import { numberToOrdinal } from "@/utils/numberToOrdinal";
-import { ws } from "@/sockets";
+// Utils
 import { isFreePlan } from "@/utils/isFreePlan";
+import { numberToOrdinal } from "@/utils/numberToOrdinal";
+
+/**
+ * @description
+ * The websocket connection.
+ * It is used to emit and listen to events.
+ * @type {Socket<ServerToClientEvents, ClientToServerEvents>}
+ */
+import { ws } from "@/sockets";
 
 // Interfaces
 interface QueueModalProps {}
@@ -24,6 +32,13 @@ interface QueueModalProps {}
  */
 export const QueueModal: FC<QueueModalProps> = (): JSX.Element => {
   /**
+   * The state of the disconnection status.
+   * It is used to determine if the user logged out while in the queue.
+   * @type {boolean}
+   */
+  const [disconnected, setDisconnected] = useState<boolean>(false);
+
+  /**
    * Use the socket context to get the queue informations.
    */
   const { queue } = useContext<ISocketContext>(SocketContext);
@@ -35,7 +50,14 @@ export const QueueModal: FC<QueueModalProps> = (): JSX.Element => {
   const { data: identity, isLoading: isIdentityLoading } =
     useGetIdentity<MeResponse>();
 
+  /**
+   * Leaves the queue based on the user's subscription plan.
+   * If the user is on the free plan, leave the shared queue.
+   * If the user is on the premium plan, leave the priority queue.
+   * @returns {void}
+   */
   const leaveQueue = (): void => {
+    // If the identity is loading or not available, stop the leave queue action.
     if (isIdentityLoading || !identity) return;
 
     if (isFreePlan(identity.user.subscription?.plan))
@@ -43,15 +65,27 @@ export const QueueModal: FC<QueueModalProps> = (): JSX.Element => {
     else ws.emit("leavePriorityQueue");
   };
 
-  if (!queue.inQueue || !identity) return <></>;
+  /**
+   * Listen for the connection and disconnection events.
+   * If the user disconnects while in the queue, set the disconnection status to true.
+   * If the user reconnects, set the disconnection status to false.
+   */
+  useEffect(() => {
+    ws.on("connect", () => setDisconnected(false));
+    ws.on("disconnect", () => setDisconnected(true));
+  }, []);
+
+  // If the user is not in the queue, the identity is loading, or the user is disconnected, hide the queue modal.
+  if (!queue.inQueue || !identity || disconnected) return <></>;
 
   return createPortal(
     <div className="z-[19] fixed bottom-16 md:bottom-5 right-2 md:right-5 w-full max-w-xs bg-n-7 p-2 rounded-lg border border-n-6/80 flex flex-col gap-2 justify-center items-center font-['Poppins']">
       <p className="text-sm">
-        {queue.positionInQueue === 1
+        {queue.positionInQueue <= 1
           ? "Please wait while we create your book"
           : `You are ${numberToOrdinal(queue.positionInQueue)} in the queue...`}
       </p>
+
       <div className="w-full rounded-full h-2.5 bg-n-5 animate-pulse">
         <div
           className="bg-gradient-to-l from-purple-600 to-blue-500 h-2.5 rounded-full transition-all ease-in-out duration-300"
@@ -67,12 +101,14 @@ export const QueueModal: FC<QueueModalProps> = (): JSX.Element => {
         ></div>
       </div>
 
-      <span
-        className="w-full text-transparent bg-gradient-to-br from-purple-600 hover:from-purple-600/80 to-blue-500 hover:to-blue-500/80 webkit-bg-clip-text inline-block text-sm tracking-wide cursor-pointer transition-all duration-300 ease-in-out text-center"
-        onClick={leaveQueue}
-      >
-        Leave Queue
-      </span>
+      {queue.positionInQueue > 1 && (
+        <span
+          className="w-full text-transparent bg-gradient-to-br from-purple-600 hover:from-purple-600/80 to-blue-500 hover:to-blue-500/80 webkit-bg-clip-text inline-block text-sm tracking-wide cursor-pointer transition-all duration-300 ease-in-out text-center"
+          onClick={leaveQueue}
+        >
+          Leave Queue
+        </span>
+      )}
     </div>,
     document.getElementById("portal") as HTMLDivElement
   );

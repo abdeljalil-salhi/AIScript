0b5fcd0232879df19d02075535fc0aa1364cd323
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-05-27 22:05:25 +0100

    Add navigation bar

--- frontend/src/pages/view/index.tsx
+++ frontend/src/pages/view/index.tsx
@@ -4,8 +4,10 @@ import { Document, Page, pdfjs } from "react-pdf";
 import { Spin } from "antd";
 import useResizeObserver from "use-resize-observer";
 
+// Dependency Styles
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons";
 
 // Interfaces
 interface ViewPageProps {}
@@ -24,16 +26,33 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
  * @exports ViewPage
  */
 export const ViewPage: FC<ViewPageProps> = (): JSX.Element => {
-  const [_, setNumPages] = useState<number>(0);
+  const [numPages, setNumPages] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(1);
   const { ref, width = 1, height = 1 } = useResizeObserver<HTMLDivElement>();
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
   };
 
-  console.log("width: ", width);
+  /**
+   * Go to the previous page of the PDF
+   * @returns {void}
+   */
+  const onPrevPage = (): void => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
 
-  console.log("height: ", height);
+  /**
+   * Go to the next page of the PDF
+   * @returns {void}
+   */
+  const onNextPage = (): void => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
 
   return (
     <div
@@ -43,18 +62,43 @@ export const ViewPage: FC<ViewPageProps> = (): JSX.Element => {
       <Document
         loading={<Spin />}
         noData={<Spin />}
-        file="https://www.abdel.codes/assets/cv_abdeljalilsalhi.pdf"
+        file={{ url: "http://localhost:3000/book/the_joy_of_intimacy.pdf" }}
         onLoadSuccess={onDocumentLoadSuccess}
+        className="relative"
       >
         <Page
           loading={<Spin />}
-          pageNumber={1}
+          pageNumber={pageNumber}
           height={width >= 625 ? height : undefined}
           width={
             width < 625 ? (width > 600 ? width - 120 : width) : undefined || 600
           }
-          renderAnnotationLayer={false}
+          renderAnnotationLayer={true}
         />
+        <div className="absolute z-[10] bottom-3.5 left-0 right-0 flex justify-center items-center text-n-1/85 text-xs font-light p-1">
+          <div className="w-3/5 flex flex-row">
+            <button
+              type="button"
+              className="w-full border-n-6/90 border-l border-t border-b text-base rounded-l-md bg-n-7/90 text-white px-4 py-1 cursor-pointer"
+              onClick={onPrevPage}
+            >
+              <ArrowLeftOutlined />
+            </button>
+            <button
+              type="button"
+              className="w-full border-n-6/90 border text-sm font-medium bg-n-7/90 flex-grow text-white px-4 py-1"
+            >
+              {pageNumber} / {numPages}
+            </button>
+            <button
+              type="button"
+              className="w-full border-n-6/90 border-t border-b border-r text-base rounded-r-md bg-n-7/90 text-white px-4 py-1 cursor-pointer"
+              onClick={onNextPage}
+            >
+              <ArrowRightOutlined />
+            </button>
+          </div>
+        </div>
       </Document>
     </div>
   );

ff03ecf5a0ac864a3fbc6228f9843c00f7ff6a8c
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-05-28 15:59:21 +0100

    Add the option of custom covers and improved image resizing

--- backend-django/api/dtos.py
+++ backend-django/api/dtos.py
@@ -1,5 +1,6 @@
 # The Data Transfer Objects (DTOs) are used to define the structure of the data that is being sent between the client and the server.
 
+
 # The BookCreateDto is used to define the structure of the data that is being sent to the server when creating a new book.
 class BookCreateDto:
     def __init__(self, data: dict) -> None:
@@ -10,3 +11,4 @@ class BookCreateDto:
         self.target_audience: str = data["target_audience"]
         self.num_chapters: int = data["num_chapters"]
         self.num_subsections: int = data["num_subsections"]
+        self.cover: str = data["cover"]
--- backend-django/model/cover_generator.py
+++ backend-django/model/cover_generator.py
@@ -1,6 +1,7 @@
-from openai import OpenAI, OpenAIError
+from os import remove
 from requests import get
-from cv2 import imread, imwrite, resize
+from openai import OpenAI, OpenAIError
+from cv2 import imread, imwrite, resize, INTER_AREA
 
 
 from .settings import API_KEY
@@ -10,42 +11,63 @@ class CoverGenerator:
     def __init__(self, book) -> None:
         self.client = OpenAI(api_key=API_KEY)
         self.book = book
-    
+
     def generate_cover(self) -> str:
-        # Define prompt
-        outline_prompt = f'"{self.book["title"]}" in the style of Vicent van Gogh with black background and dark colors, high quality, only using dark colors, and a dark, mysterious, and eerie atmosphere.'
-        
-        try:
-            # Call API
-            response = self.client.images.generate(
-                model="dall-e-2",
-                prompt=outline_prompt,
-                size="512x512",
-                n=1,
-            )
-            image_url = response.data[0].url
-        except OpenAIError as e:
-            raise e
+        # Check if the cover is not AI generated
+        if self.book["cover"] != "ai":
+            image_url = self.book["cover"]
+        # If the cover is AI generated use the API to generate a new one
+        else:
+            # Define prompt
+            outline_prompt = f'"{self.book["title"]}" in the style of Vicent van Gogh with black background and dark colors, high quality, only using dark colors, and a dark, mysterious, and eerie atmosphere.'
+
+            try:
+                # Call API
+                response = self.client.images.generate(
+                    model="dall-e-2",
+                    prompt=outline_prompt,
+                    size="512x512",
+                    n=1,
+                )
+                image_url = response.data[0].url
+            except OpenAIError as e:
+                raise e
 
         # Save the image to {id}_generated.png
         image = get(image_url).content
-        with open(f"public/covers/{self.book['id']}_generated.png", "wb+") as file:
+        with open(f"media/covers/{self.book['id']}_generated.png", "wb+") as file:
             file.write(image)
-        
+
         # Resize the image
-        image = imread(f"public/covers/{self.book['id']}_generated.png")
+        image = imread(f"media/covers/{self.book['id']}_generated.png")
         self.resize_image(image)
-        
+
         # Return the path to the final generated cover
-        return f"public/covers/{self.book['id']}.png"
+        return f"media/covers/{self.book['id']}.png"
 
     def resize_image(self, image) -> None:
-        # Resize the image to 1250x1250 first to save the aspect ratio
-        image = resize(image, (1250, 1250))
-        imwrite(f"public/covers/{self.book['id']}_resized.png", image)
-        
+        original_height, original_width = image.shape[:2]
+        new_height = 1250
+        new_width = int(original_width * (new_height / original_height))
+
+        if new_width < 1000:
+            new_width = 1000
+            new_height = int(original_height * (new_width / original_width))
+
+        # Resize the image to the new dimensions
+        image = resize(image, (new_width, new_height), interpolation=INTER_AREA)
+
+        # Calculate the cropping box (center crop to 1000x1250)
+        left = (new_width - 1000) // 2
+        right = left + 1000
+        top = (new_height - 1250) // 2
+        bottom = top + 1250
+
         # Crop the image to 1000x1250 from the center to fit the cover
-        image = image[:, 125:1125]
-        
+        image = image[top:bottom, left:right]
+
+        # Delete the generated image
+        remove(f"media/covers/{self.book['id']}_generated.png")
+
         # Save the final image to {id}.png
-        imwrite(f"public/covers/{self.book['id']}.png", image)
+        imwrite(f"media/covers/{self.book['id']}.png", image)

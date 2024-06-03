e8082347f0d58a43790e5a3c8d76db3ba3a38cb7
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-06-03 19:25:07 +0100

    Add metadata for the platform create page

--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,5 +1,7 @@
 // Dependencies
 import { FC } from "react";
+import { Helmet } from "react-helmet-async";
+import { useDocumentTitle } from "@refinedev/react-router-v6";
 
 // Components
 import { CreateForm } from "./CreateForm";
@@ -16,8 +18,18 @@ interface CreatePageProps {}
  * @exports CreatePage
  */
 export const CreatePage: FC<CreatePageProps> = (): JSX.Element => {
+  useDocumentTitle("AIScript - Create a New Book");
+
   return (
     <>
+      <Helmet>
+        <title>AIScript - Create a New Book</title>
+        <meta
+          name="description"
+          content="Create a new book with AIScript. Display the books you have created and manage them with ease. Join the future of book creation today."
+        />
+      </Helmet>
+
       <div className="w-full flex flex-col md:flex-row">
         <div className="w-full md:w-2/5 min-h-0 h-[50vh] md:min-h-screen border-b md:border-r border-n-6/70 p-4 sticky md:static top-0 overflow-y-auto">
           <CreateForm />

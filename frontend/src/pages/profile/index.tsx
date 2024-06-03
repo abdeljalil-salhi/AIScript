fa5110c18f3d2ff491c27a1f0360de4dbfff0d9d
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-06-03 19:29:55 +0100

    Add metadata for the platform profile page

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,5 +1,7 @@
 // Dependencies
 import { FC } from "react";
+import { Helmet } from "react-helmet-async";
+import { useDocumentTitle } from "@refinedev/react-router-v6";
 
 // Components
 import { ProfilePicture } from "./ProfilePicture";
@@ -16,14 +18,26 @@ interface ProfilePageProps {}
  * @exports ProfilePage
  */
 export const ProfilePage: FC<ProfilePageProps> = (): JSX.Element => {
+  useDocumentTitle("AIScript - Your Profile");
+
   return (
-    <div className="w-full md:h-full flex flex-col md:flex-row justify-center gap-6 lg:gap-32 overflow-y-auto p-4 md:p-6 lg:p-16 font-['Poppins']">
-      <div className="w-full md:w-2/5 flex flex-col items-center lg:items-end justify-start pt-20 md:pt-6">
-        <ProfilePicture />
+    <>
+      <Helmet>
+        <title>AIScript - Your Profile</title>
+        <meta
+          name="description"
+          content="Manage your AIScript profile and take control of your book creation journey. Access your settings, preferences, and past creations effortlessly."
+        />
+      </Helmet>
+
+      <div className="w-full md:h-full flex flex-col md:flex-row justify-center gap-6 lg:gap-32 overflow-y-auto p-4 md:p-6 lg:p-16 font-['Poppins']">
+        <div className="w-full md:w-2/5 flex flex-col items-center lg:items-end justify-start pt-20 md:pt-6">
+          <ProfilePicture />
+        </div>
+        <div className="w-full md:w-3/5 h-full flex flex-col items-center lg:items-start justify-start gap-6 p-4 md:pt-6">
+          <SettingsForm />
+        </div>
       </div>
-      <div className="w-full md:w-3/5 h-full flex flex-col items-center lg:items-start justify-start gap-6 p-4 md:pt-6">
-        <SettingsForm />
-      </div>
-    </div>
+    </>
   );
 };

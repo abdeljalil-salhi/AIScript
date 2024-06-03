31533db874abf4160553cca597da460418056dc4
Abdeljalil Salhi <abdousalhi868@gmail.com>
2024-06-03 19:26:11 +0100

    Add metadata for the platform library page

--- frontend/src/pages/library/index.tsx
+++ frontend/src/pages/library/index.tsx
@@ -1,5 +1,7 @@
 // Dependencies
 import { FC } from "react";
+import { Helmet } from "react-helmet-async";
+import { useDocumentTitle } from "@refinedev/react-router-v6";
 
 // Components
 import { Header } from "./Header";
@@ -16,10 +18,22 @@ interface LibraryPageProps {}
  * @exports LibraryPage
  */
 export const LibraryPage: FC<LibraryPageProps> = (): JSX.Element => {
+  useDocumentTitle("AIScript - Your Library");
+
   return (
-    <div className="w-full h-[calc(100vh-3.5rem)] md:h-screen flex flex-col gap-6 overflow-y-auto">
-      <Header />
-      <ListDisplay />
-    </div>
+    <>
+      <Helmet>
+        <title>AIScript - Your Library</title>
+        <meta
+          name="description"
+          content="Manage your books with ease. Access your books from anywhere and anytime."
+        />
+      </Helmet>
+
+      <div className="w-full h-[calc(100vh-3.5rem)] md:h-screen flex flex-col gap-6 overflow-y-auto">
+        <Header />
+        <ListDisplay />
+      </div>
+    </>
   );
 };
